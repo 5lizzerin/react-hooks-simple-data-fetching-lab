62fa902eb22f00123cfd9c0c5c1c804941596c50
Lizz <lizzcklein@gmail.com>
2023-10-08 12:23:01 -0400

    dsafsd

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React, {useEffect, useState} from "react";
+
+const dogUrl = "https://dog.ceo/api/breeds/image/random"
+
+function App () {
+    const [dogPic, setDogPic] = useState(null)
+    useEffect( () => {
+        fetch(dogUrl)
+        .then(dogUrlServerResponse => dogUrlServerResponse.json())
+        .then(dogJson => {
+            console.log(dogJson.message)
+            setDogPic(dogJson.message)
+        });
+    }, []);
+
+            if (!dogPic) return <p>Loading...</p>;
+
+            return <img src={dogPic} alt="a dog"></img>
+}
+
+export default App;
